d612d674cdef07fe637714475cdbf23778e1b4cb
mdkaifjmi <141780387+mdkaifjmi@users.noreply.github.com>
2026-06-16 23:29:04 +0530

    Create RemoveNNode.java

---
+++ Week3/Day02/RemoveNNode.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // move fast n+1 steps
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // remove node
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}

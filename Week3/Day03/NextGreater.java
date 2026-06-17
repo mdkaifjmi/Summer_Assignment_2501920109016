6cab466418e6c83a54aad6c89ad519d298206a61
mdkaifjmi <141780387+mdkaifjmi@users.noreply.github.com>
2026-06-17 23:15:00 +0530

    Create NextGreater.java

---
+++ Week3/Day03/NextGreater.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Find next greater element for nums2
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        // Fill answer for nums1
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}

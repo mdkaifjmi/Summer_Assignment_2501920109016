deb6246e9e1e46f9d5048f15fb6953717d54f293
mdkaifjmi <141780387+mdkaifjmi@users.noreply.github.com>
2026-06-26 02:48:27 +0530

    Create Constructbinary.java

---
+++ Week4/Day05/Constructbinary.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+class Solution {
+
+    private int preIndex = 0;
+    private HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int left, int right) {
+
+        if (left > right)
+            return null;
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int index = map.get(rootVal);
+
+        root.left = build(preorder, left, index - 1);
+        root.right = build(preorder, index + 1, right);
+
+        return root;
+    }
+}

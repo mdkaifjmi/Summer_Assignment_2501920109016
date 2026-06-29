4b675458ef7e6111061ac6a699abfc989bfa547a
mdkaifjmi <141780387+mdkaifjmi@users.noreply.github.com>
2026-06-29 22:40:14 +0530

    Add files via upload

---
+++ Week4/OOPs/Outer.java
@@ -0,0 +1,14 @@
+public class Outer {
+
+    void display() {
+        System.out.println("Display method of Outer class");
+    }
+
+    class Inner {
+
+        void display() {
+            System.out.println("Display method of Inner class");
+        }
+
+    }
+}
